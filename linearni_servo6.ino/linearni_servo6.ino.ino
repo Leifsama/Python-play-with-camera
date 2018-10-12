5e8bd898fc006ed575e8dc0c02babb1172da2e4c
Leifsama <35638361+Leifsama@users.noreply.github.com>
2018-10-12 12:05:46 +0200

    Add files via upload

---
+++ linearni_servo6.ino/linearni_servo6.ino.ino
@@ -0,0 +1,87 @@
+#include <Servo.h>
+
+Servo motor_predni;        //initialize a servo object for the connected servo  
+Servo motor_zadni;                
+int angle = 0;
+//int vysuv_predni = 0;   
+int incomingByte = 0;   
+//int magnet1 = 0;
+
+void setup() 
+{ 
+  Serial.begin(9600);
+  
+  motor_predni.attach(9);      //pin 3 attach the signal pin of servo to pin9 of arduino
+  motor_zadni.attach(6);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+
+  Serial.println("aaa");
+} 
+  
+void loop() 
+{
+    
+    while(Serial.available())
+    {
+    int vysuv_predni = Serial.parseInt();
+    
+    
+  //digitalWrite(magnet1, HIGH);
+   while(vysuv_predni == 0)
+{
+   vysuv_predni = Serial.parseInt();
+
+   Serial.println(vysuv_predni);
+}
+   
+    int vysuv_zadni = Serial.parseInt();
+    int magnetA = Serial.parseInt();
+    int magnetB = Serial.parseInt();
+  
+
+  Serial.println("Ve formatu 0-100 (motor predni) 0-100 (motor zadni) 0-1 (magnet A) 0-1 (magnet B)");
+  Serial.print("Motor vpredu:");
+  Serial.println(vysuv_predni);
+  Serial.print("Motor vzadu:");
+  Serial.println(vysuv_zadni);
+  Serial.print("Magnet A:");
+  Serial.println(magnetA);
+  Serial.print("Magnet B:");
+  Serial.println(magnetB);
+
+
+    
+  
+    int vysuv_predni2 = vysuv_predni+50;
+    int vysuv_zadni2 = vysuv_zadni+50;  
+                       
+//  Serial.println(str); 
+    motor_predni.write(vysuv_predni2);       //command to rotate the servo to the specified angle
+   
+    //delay(50);   
+
+    motor_zadni.write(vysuv_zadni2);                 //command to rotate the servo to the specified angle
+    //delay(50);
+       
+    
+    if (magnetA >= 1)
+    digitalWrite(12, HIGH);
+      else
+      digitalWrite(12, LOW);
+
+    
+   //if (magnetA = 0)
+   // digitalWrite(13, HIGH);
+
+   if (magnetB >= 1)
+    digitalWrite(11, HIGH);
+    else
+    digitalWrite(11, LOW);
+
+    
+      
+  } 
+      
+
+}
